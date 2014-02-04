94415d28eaf6fdec466c17fa0ca82f7b0e4b1531
MasterGberry <adam@tinywebteam.com>
2014-02-03 23:39:39 -0800

    Added permissions, base bluetooth LE check code, and forced SDK version to be 18 (min version for Bluetooth LE support)

--- BluetoothApp/src/main/java/edu/ucsb/cs/capstone/letmypeoplecode/dowsingrod/DetectionLauncher.java
+++ BluetoothApp/src/main/java/edu/ucsb/cs/capstone/letmypeoplecode/dowsingrod/DetectionLauncher.java
@@ -1,10 +1,16 @@
 package edu.ucsb.cs.capstone.letmypeoplecode.dowsingrod;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import  edu.ucsb.cs.capstone.letmypeoplecode.dowsingrod.util.SystemUiHider;
 
@@ -16,6 +22,8 @@ import  edu.ucsb.cs.capstone.letmypeoplecode.dowsingrod.util.SystemUiHider;
  */
 public class DetectionLauncher extends Activity {
     Handler handler = new Handler();
+    private BluetoothAdapter mBluetoothAdapter;
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,23 @@ public class DetectionLauncher extends Activity {
         setContentView(R.layout.activity_detection_launcher);
         final View contentView = findViewById(R.id.fullscreen_content);
 
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
     public void genSound(View view) {
 
